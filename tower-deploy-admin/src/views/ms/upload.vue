73c33188151888259f3255985b6ede1623bbc794
zhenpeng <1050244110@qq.com>
2018-06-14 13:38:38 +0800

    003: design the web. onSuccess onError.

--- tower-deploy-admin/src/views/ms/upload.vue
+++ tower-deploy-admin/src/views/ms/upload.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="app-container">
 
-    <el-form ref="form" :model="form" label-width="120px">
+    <el-form ref="form" :model="form" label-width="120px" :rules="rules">
 
-      <el-form-item label="服務名稱">
+      <el-form-item label="服務名稱" prop="name">
         <el-input v-model="form.name"></el-input>
       </el-form-item>
-      <el-form-item label="提交信息">
+      <el-form-item label="提交信息" prop="commitMessage">
         <el-input v-model="form.commitMessage"></el-input>
       </el-form-item>
 
@@ -17,7 +17,9 @@
           v-bind:action="uploadUrl"
           :on-preview="handlePreview"
           :on-remove="handleRemove"
-          :file-list="fileList"
+          :on-success="onSuccess"
+          :on-error="onError"
+          :file-list="form.fileList"
           :auto-upload="false">
           <el-button slot="trigger" size="small" type="success">选取文件</el-button>
         </el-upload>
@@ -41,9 +43,16 @@
       return {
         form: {
           name: '',
-          commitMessage: ''
+          commitMessage: '',
+          fileList: []
         },
-        fileList: []
+        rules: {
+          name: [
+            {required: true, message: '请输入服務名称', trigger: 'blur'}],
+
+          commitMessage: [
+            {required: true, message: '请輸入提交信息', trigger: 'blur'}]
+        }
       }
     },
     computed: {
@@ -53,8 +62,14 @@
     },
     methods: {
       onSubmit() {
-        console.log(this.uploadUrl);
-        this.$refs.upload.submit();
+        this.$refs["form"].validate((valid) => {
+          if (valid) {
+            this.$refs.upload.submit();
+          } else {
+            this.$message('驗證失敗！');
+            return false;
+          }
+        });
       },
       onCancel() {
         this.$router.push('ms');
@@ -64,6 +79,13 @@
       },
       handlePreview(file) {
         console.log(file);
+      },
+      onSuccess(response, file, fileList) {
+        this.$message('保存成功！');
+        this.$router.push('ms');
+      },
+      onError(err, file, fileList) {
+        this.$message('保存失敗！' + err);
       }
     }
   }
